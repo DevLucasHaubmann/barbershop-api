4189cc6441107f0418b5482c58fdbc737dc1c3a7
Lucas Haubmann <lucas.a.haubmann@gmail.com>
2026-08-10 11:28:24 -0300

    fix: Annotations were refactored.

--- api/src/main/java/com/haubmannlucas/barbershop/api/domain/user/UserEntity.java
+++ api/src/main/java/com/haubmannlucas/barbershop/api/domain/user/UserEntity.java
@@ -6,11 +6,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import org.hibernate.annotations.Audited;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity(name = "Users")
-@Audited.Table(name = "users")
+@Table(name = "users")
 public class UserEntity {
 
     @Id
